157db2ab0d54edca281f14826937d6c77096facc
Emanuel Rukavina <rukanuel@gmail.com>
2024-11-01 00:55:32 +0100

    Add flags to bin & Kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -18,17 +18,33 @@ class Kernel
     private string $directory;
     private string $configPath;
     private $config;
+    private array $args;
 
-    public function __construct()
+    public function __construct(array $args = [])
     {
+        $this->args = $args;
         $this->directory = getcwd();
         $this->configPath = getcwd() . '/symdoc.ini';
     }
     public function run(): void
     {
+        // Prepare for flags func.
+        foreach ($this->args as $arg) {
+            if ($arg === '--flag1') {
+                echo "Flag1 is set!" . PHP_EOL;
+            }
+
+            if (str_starts_with($arg, '--flag2=')) {
+                $flag2Value = explode('=', $arg)[1];
+                echo "Flag2 is set with value: $flag2Value" . PHP_EOL;
+            }
+        }
+
+        // Make this into a class of its ow so it can be used across the project
         if (!file_exists($this->configPath)) {
             mkdir(dirname($this->configPath), 0777, true);
-            copy(__DIR__ ."/resources/symdoc.ini", $this->configPath);
+            // TODO swith version with actual version
+            copy(__DIR__ . "/_resources/symdoc.ini", $this->configPath);
         } 
         $config = parse_ini_file($this->configPath);
         $directory = getcwd();
